43864246f5874883dab6efbdc2ddaf374e1c795b
Rafael Gutkowski <nounderline@libred.org>
2024-09-28 21:22:38 +0200

    Don't detect nvim config changes

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -26,7 +26,9 @@ require("lazy").setup({
     version = false, -- always use the latest git commit
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
+  change_detection = {
+    enabled = false,
+  },
   checker = { enabled = false }, -- automatically check for plugin updates
   performance = {
     rtp = {
