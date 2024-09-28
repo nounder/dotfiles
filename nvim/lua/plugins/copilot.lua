968b5b4404c94a17e5dc36db7c553afcec79d148
Rafael Gutkowski <nounderline@libred.org>
2024-09-28 17:28:23 +0200

    Remove avante

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,43 +1,5 @@
 return {
   {
-    "yetone/avante.nvim",
-    enabled = true,
-    event = "VeryLazy",
-    lazy = false,
-    opts = {
-      ignore_filetypes = {
-        markdown = true,
-      },
-      hints = {
-        enabled = false,
-      },
-    },
-    build = ":AvanteBuild", -- This is optional, recommended tho. Also note that this will block the startup for a bit since we are compiling bindings in Rust.
-    dependencies = {
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-    },
-  },
-
-  {
     "supermaven-inc/supermaven-nvim",
     cmd = {
       "SupermavenStart",
