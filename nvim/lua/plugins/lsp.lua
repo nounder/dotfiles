f64b9042f9b304d18a87bf29c7582be6dc3890f8
Rafael Gutkowski <nounderline@libred.org>
2024-09-28 21:23:02 +0200

    Setup LSP for svelte, typescript, and json

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -10,6 +10,28 @@ return {
         virtual_text = false,
         signs = false,
       },
+      servers = {
+        svelte = {
+          --cmd = { "deno", "run", "-E", "-R", "-S=cpus,homedir,uid", "--allow-run", "--allow-write=.", "--allow-ffi", "npm:svelte-language-server", "--stdio" }
+          --cmd = { "deno", "run", "-A", "npm:svelte-language-server", "--stdio" }
+          --cmd = { "bunx", "--bun", "svelte-language-server", "--stdio" }
+          cmd = { "npx", "svelte-language-server", "--stdio" }
+        },
+        vtsls = {
+          --cmd = { "bunx", "--bun", "@vtsls/language-server", "--stdio" },
+          cmd = { "npx", "@vtsls/language-server", "--stdio" },
+          settings = {
+            typescript = {
+              preferences = {
+                importModuleSpecifierEnding = "minimal",
+              }
+            }
+          }
+        },
+        jsonls = {
+          cmd = { "npx", "vscode-json-languageserver", "--stdio" }
+        }
+      }
     },
   },
 }
