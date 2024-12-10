4e36283abcc03a2f30a61119c3b595eda49f1e44
Rafael Gutkowski <nounderline@libred.org>
2024-12-10 20:18:11 +0700

    nvim: use different keybindings for editor and ai completions

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -6,7 +6,18 @@ return {
     ---@type blink.cmp.Config
     opts = {
       keymap = {
-        preset = "super-tab",
+        preset = "default",
+
+        ["<C-e>"] = { "hide" },
+        ["<C-l>"] = { "select_and_accept" },
+        ["<C-;>"] = {
+          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
+        },
+        ["<C-k>"] = { "select_prev", "fallback" },
+        ["<C-j>"] = { "select_next", "fallback" },
+
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       },
     },
   },
