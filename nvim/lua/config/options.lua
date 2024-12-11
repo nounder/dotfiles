af1d12110c5b0339561f9ad2477c459cba63bfb7
Rafael Gutkowski <nounderline@libred.org>
2024-12-12 03:58:39 +0700

    add neovide config and update status line

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -32,3 +32,25 @@ vim.opt.mousescroll = "ver:1,hor:0"
 vim.g.lazyvim_prettier_needs_config = true
 
 vim.opt.statuscolumn = ""
+
+vim.o.statusline = "%t:%l"
+
+-- More:
+-- https://neovide.dev/configuration.html
+if vim.g.neovide then
+  vim.g.neovide_cursor_animation_length = 0
+  vim.g.neovide_scroll_animation_length = 0
+  vim.g.neovide_window_blurred = true
+
+  vim.g.neovide_transparency = 0.4
+
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+
+  vim.g.neovide_floating_shadow = true
+  vim.g.neovide_floating_z_height = 10
+  vim.g.neovide_light_angle_degrees = 45
+  vim.g.neovide_light_radius = 5
+
+  vim.g.neovide_show_border = true
+end
