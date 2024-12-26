f17317ad7fa970d20f8e1b35d77a6b263cc0ccdb
Rafael Gutkowski <nounderline@libred.org>
2024-12-26 23:38:54 +0700

    nvim: customize fzf
    thanks tom!
    
    https://www.reddit.com/r/neovim/comments/1hhiidm/a_few_nice_fzflua_configurations_now_that_lazyvim/

---
+++ nvim/lua/plugins/fzf.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "ibhagwan/fzf-lua",
+    opts = {
+      oldfiles = {
+        include_current_session = true,
+      },
+      previewers = {
+        builtin = {
+          syntax_limit_b = 1024 * 100, -- 100KB
+        },
+      },
+      grep = {
+        -- With this change, I can sort of get the same behaviour in live_grep.
+        -- ex: > enable --*/plugins/*
+        -- I still find this a bit cumbersome. There's probably a better way of doing this.
+        -- filter results by a glob, eg. "enabled = false -- */plugins/*"
+        rg_glob = true, -- enable glob parsing
+        glob_flag = "--iglob", -- case insensitive globs
+        glob_separator = "%s%-%-", -- query separator pattern (lua): ' --'
+      },
+    },
+
+    keys = {
+      -- Keybinding for searching within the current buffer
+      {
+        "<leader>fb",
+        function()
+          require("fzf-lua").grep_curbuf()
+        end,
+        desc = "Search in Current Buffer",
+      },
+    },
+  },
+}
