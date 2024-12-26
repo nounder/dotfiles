d3f5d564c4fb4e20edf0708fc7b3d179439f4503
Rafael Gutkowski <nounderline@libred.org>
2024-12-26 23:43:01 +0700

    fish: cleanup comps

--- fish/completions/nomad.fish
+++ fish/completions/nomad.fish
@@ -3,7 +3,6 @@ function __complete_nomad
     set -lx COMP_LINE (commandline -cp)
     test -z (commandline -ct)
     and set COMP_LINE "$COMP_LINE "
-    /opt/homebrew/bin/nomad
+    nomad
 end
 complete -f -c nomad -a "(__complete_nomad)"
-
