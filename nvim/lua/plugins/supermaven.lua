523feac7f9c7fb32d4e3b5b289601cb4d24fca4b
Rafael Gutkowski <nounderline@libred.org>
2025-01-10 16:10:37 -0600

    Manually enable Supermaven

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -1,8 +1,10 @@
 return {
   {
     "supermaven-inc/supermaven-nvim",
+    enabled = function()
+      return os.getenv("NVIM_AI_ENABLED") == "true"
+    end,
     opts = {
-
       ignore_filetypes = {
         markdown = true,
         text = true,
