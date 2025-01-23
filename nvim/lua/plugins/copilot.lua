c4e8f3bf9a3c86d5c4f37ef90d8ac019bb7eb156
Rafael Gutkowski <nounderline@libred.org>
2025-01-23 17:15:08 -0600

    nvim: opt in for copilot

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      filetypes = {
+        ["."] = false,
+        javascript = true,
+        typescript = true,
+        sh = true,
+        lua = true,
+        bash = true,
+        fish = true,
+        go = true,
+        zig = true,
+        rust = true,
+      },
+      -- copilot_node_command = "node-deno",
+    },
+  },
+}
