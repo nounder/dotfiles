9ad23c665f818b3d2cc2d47709871561efd8a667
Rafael Gutkowski <nounderline@libred.org>
2025-02-21 21:01:46 -0700

    Migrate to Snacks.picker

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,3 +1,11 @@
+local function sim(chars)
+  local keys = vim.api.nvim_replace_termcodes(chars, true, false, true)
+
+  return function()
+    vim.api.nvim_feedkeys(keys, "m", true)
+  end
+end
+
 return {
   {
     "folke/snacks.nvim",
@@ -6,9 +14,18 @@ return {
       dashboard = {
         enabled = false,
       },
-      win = {
-        border = "rounded",
-      },
+    },
+    keys = {
+      { "ff", sim("<leader>ff"), expr = true },
+      { "fr", sim("<leader>fr"), expr = true },
+
+      { "<leader>fo", "<cmd>FzfLua resume<cr>", desc = "Resume" },
+      { "fo", sim("<leader>fo"), expr = true },
+
+      { "<leader>fl", "<cmd>FzfLua grep_curbuf<cr>", desc = "Search in Current Buffer" },
+      { "fl", sim("<leader>fl"), expr = true },
+
+      { "gf", "<cmd>FzfLua lsp_finder<cr>" },
     },
   },
 }
