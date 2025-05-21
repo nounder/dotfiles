84f66077f1381fce930b3699419ea79b52426db0
Rafael Gutkowski <nounderline@libred.org>
2025-05-21 16:09:28 -0400

    Move to oil fro mini.files

--- nvim/lua/plugins/minifiles.lua
+++ nvim/lua/plugins/minifiles.lua
@@ -1,5 +1,6 @@
 return {
   "echasnovski/mini.files",
+  enabled = false,
   opts = {
     options = {
       use_as_default_explorer = true,
@@ -21,19 +22,19 @@ return {
       desc = "Open mini.files (cwd)",
     },
 
-    {
-      "fm",
-      function()
-        require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-      end,
-      desc = "Open mini.files (Directory of Current File)",
-    },
-    {
-      "fM",
-      function()
-        require("mini.files").open(vim.uv.cwd(), true)
-      end,
-      desc = "Open mini.files (cwd)",
-    },
+    -- {
+    --   "fm",
+    --   function()
+    --     require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+    --   end,
+    --   desc = "Open mini.files (Directory of Current File)",
+    -- },
+    -- {
+    --   "fM",
+    --   function()
+    --     require("mini.files").open(vim.uv.cwd(), true)
+    --   end,
+    --   desc = "Open mini.files (cwd)",
+    -- },
   },
 }
---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,33 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      default_file_explorer = true,
+      columns = {
+        "icon",
+        "size",
+      },
+      delete_to_trash = true,
+      float = {
+        max_width = 0.8,
+        max_height = 0.9,
+      },
+    },
+    keys = {
+      {
+        "<space>fm",
+        function()
+          require("oil").open_float(nil, {
+            preview = {
+              horziontal = true,
+            },
+          })
+        end,
+      },
+      {
+        "<space>fM",
+        "<cmd>Oil<cr>",
+      },
+    },
+  },
+}
