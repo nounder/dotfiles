fc6e9a2d9d52b969e4ac94076a0626f351f2a44b
Rafael Gutkowski <nounderline@libred.org>
2025-07-02 21:22:50 -0400

    remove atuin

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Get the absolute path to the dotfiles directory
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 # if -f option is passed, force symlink
 if [ "$1" = "-f" ]; then
   LN_OPTS="-f"
@@ -11,19 +14,17 @@ mkdir -p ~/.config
 
 cd ~/.config
 
-ln -s $LN_OPTS ../dotfiles/kitty/ kitty
+ln -s $LN_OPTS "$DOTFILES_DIR/kitty/" kitty
 
-ln -s $LN_OPTS ../dotfiles/atuin/ atuin
+ln -s $LN_OPTS "$DOTFILES_DIR/fish/" fish
 
-ln -s $LN_OPTS ../dotfiles/fish/ fish
+ln -s $LN_OPTS "$DOTFILES_DIR/helix/" helix
 
-ln -s $LN_OPTS ../dotfiles/helix/ helix
+ln -s $LN_OPTS "$DOTFILES_DIR/nvim/" nvim
 
-ln -s $LN_OPTS ../dotfiles/nvim/ nvim
+ln -s $LN_OPTS "$DOTFILES_DIR/ghostty/" ghostty
 
-ln -s $LN_OPTS ../dotfiles/ghostty/ ghostty
+ln -s $LN_OPTS "$DOTFILES_DIR/tmux.conf" .tmux.conf
 
-ln -s $LN_OPTS ../dotfiles/tmux.conf .tmux.conf
-
-mkdir -p direnv; ln -s $LN_OPTS ../../dotfiles/direnv.toml direnv/direnv.toml
-
+mkdir -p direnv
+ln -s $LN_OPTS "$DOTFILES_DIR/direnv.toml" direnv/direnv.toml
