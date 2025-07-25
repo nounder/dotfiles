d2e3687180f3f71dc483e706aef935e210632d99
Rafael Gutkowski <nounderline@libred.org>
2025-07-25 17:30:37 -0400

    nvim: enable prettier

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,27 +1,41 @@
-return {
-  "stevearc/conform.nvim",
-  opts = {
-    formatters_by_ft = {
-      markdown = { "dprint" },
-      html = { "dprint" },
-      python = { "dprint" },
-      sql = { "pg_format" },
-      svelte = { "dprint" },
-      javascript = { "dprint" },
-      typescript = { "dprint" },
-      javascriptreact = { "dprint" },
-      typescriptreact = { "dprint" },
-      json = { "dprint" },
-      toml = { "dprint" },
-      fish = { "fish_indent" },
-      sh = { "shfmt" },
-    },
+local dprint_supported = {
+  "markdown",
+  "html",
+  "python",
+  "svelte",
+  "javascript",
+  "typescript",
+  "javascriptreact",
+  "typescriptreact",
+  "json",
+  "toml",
+}
 
-    formatters = {
-      dprint = {
+return {
+  {
+    "stevearc/conform.nvim",
+    opts = function(_, opts)
+      opts.formatters_by_ft = opts.formatters_by_ft or {}
+      for _, ft in ipairs(dprint_supported) do
+        opts.formatters_by_ft[ft] = opts.formatters_by_ft[ft] or {}
+        table.insert(opts.formatters_by_ft[ft], "dprint")
+      end
+
+      opts.formatters = opts.formatters or {}
+      opts.formatters.dprint = {
         condition = function(ctx)
           return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[1]
         end,
+      }
+    end,
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "pg_format" },
+        fish = { "fish_indent" },
+        sh = { "shfmt" },
       },
     },
   },
