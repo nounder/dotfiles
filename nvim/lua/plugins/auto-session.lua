7ed97d25255ce8b7aa4401f91c05f5a6341f3b9a
Rafael Gutkowski <nounderline@libred.org>
2025-11-16 21:59:54 +0100

    pimp diffview

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,9 +1,14 @@
 return {
-  -- https://github.com/rmagatti/auto-session
-  "rmagatti/auto-session",
-  enabled = true,
-  lazy = false,
-  config = function()
-    require("auto-session").setup({})
-  end,
+	-- https://github.com/rmagatti/auto-session
+	"rmagatti/auto-session",
+	enabled = true,
+	lazy = false,
+	opts = {
+		suppressed_dirs = { "~/", "~/Downloads", "/" },
+		-- Don't save diffview buffers
+		bypass_save_filetypes = {
+			"DiffviewFiles",
+			"DiffviewFileHistory",
+		},
+	},
 }
