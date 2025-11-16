bceacdbfc2db8e04370cb4680a8ec3cb70ac2ba8
Rafael Gutkowski <nounderline@libred.org>
2025-11-16 21:59:54 +0100

    pimp diffview

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -44,6 +44,8 @@ return {
   {
     -- https://github.com/sindrets/diffview.nvim
     "sindrets/diffview.nvim",
+    -- cannot be lazy because Snacks overwrittes keybinding here
+    lazy = false,
     cmd = {
       "DiffviewOpen",
       "DiffviewClose",
@@ -53,7 +55,74 @@ return {
     },
     opts = {
       enhanced_diff_hl = true,
+      keymaps = {
+        view = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+        },
+        file_panel = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+        },
+        file_history_panel = {
+          { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+        },
+      },
+      hooks = {
+        diff_buf_read = function()
+          -- Disable swap files for diff buffers to avoid E325 errors
+          vim.opt_local.swapfile = false
+          -- Disable folding by default
+          vim.opt_local.foldenable = false
+        end,
+        view_opened = function()
+          -- Set tab name when diffview opens
+          vim.cmd("silent! file <diffview>")
+
+          -- Focus on the main diff window
+          local view = require("diffview.lib").get_current_view()
+          if view and view.cur_layout then
+            local main_win = view.cur_layout:get_main_win()
+            if main_win then
+              vim.api.nvim_set_current_win(main_win.id)
+            end
+          end
+        end,
+      },
     },
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>" },
+    },
+    init = function()
+      -- Disable highlight for added and modified lines,
+      -- and underline changed text
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, "DiffAdd", { bg = "none", fg = "none" })
+          vim.api.nvim_set_hl(0, "DiffChange", { bg = "none", fg = "none" })
+          vim.api.nvim_set_hl(0, "DiffText", { bg = "none", fg = "none", underline = true })
+        end,
+      })
+
+      -- Close all diffview buffers before exiting nvim
+      vim.api.nvim_create_autocmd("VimLeavePre", {
+        pattern = "*",
+        callback = function()
+          -- Iterate over all buffers to find diffview buffers
+          for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+            if vim.api.nvim_buf_is_valid(buf) then
+              local ft = vim.bo[buf].filetype
+              -- Check if buffer is a diffview buffer
+              if ft == "DiffviewFiles" or ft == "DiffviewFileHistory" then
+                -- Switch to the diffview buffer and close it
+                vim.api.nvim_set_current_buf(buf)
+                vim.cmd("DiffviewClose")
+                break
+              end
+            end
+          end
+        end,
+      })
+    end,
   },
 
   {
@@ -67,6 +136,7 @@ return {
   {
     -- https://github.com/tpope/vim-fugitive
     "tpope/vim-fugitive",
+    enabled = true,
     keys = {
       {
         "<leader>gb",
@@ -76,10 +146,10 @@ return {
         "<leader>gc",
         "<cmd>G commit<CR>",
       },
-      {
-        "<leader>gd",
-        "<cmd>G diff<CR>",
-      },
+      -- {
+      --   "<leader>gd",
+      --   "<cmd>G diff<CR>",
+      -- },
       {
         "<leader>gl",
         "<cmd>G log<CR>",
@@ -137,11 +207,6 @@ return {
       {
         "<leader>gll",
         Snacks.picker.git_log_line,
-        desc = "Git pull",
-      },
-      {
-        "<leader>gd",
-        Snacks.picker.git_diff,
         desc = "Git pull",
       },
     },
