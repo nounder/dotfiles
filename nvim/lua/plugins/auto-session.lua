a6428b83100d191b7ab90653893038bdb55bd612
Rafael Gutkowski <nounderline@libred.org>
2025-11-18 14:54:38 +0100

    don't use auto-session args are passed

--- nvim/lua/plugins/auto-session.lua
+++ nvim/lua/plugins/auto-session.lua
@@ -1,14 +1,14 @@
 return {
-	-- https://github.com/rmagatti/auto-session
-	"rmagatti/auto-session",
-	enabled = true,
-	lazy = false,
-	opts = {
-		suppressed_dirs = { "~/", "~/Downloads", "/" },
-		-- Don't save diffview buffers
-		bypass_save_filetypes = {
-			"DiffviewFiles",
-			"DiffviewFileHistory",
-		},
-	},
+  -- https://github.com/rmagatti/auto-session
+  "rmagatti/auto-session",
+  enabled = #vim.v.argv <= 1,
+  lazy = false,
+  opts = {
+    suppressed_dirs = { "~/", "~/Downloads", "/" },
+    -- Don't save diffview buffers
+    bypass_save_filetypes = {
+      "DiffviewFiles",
+      "DiffviewFileHistory",
+    },
+  },
 }
