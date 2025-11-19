11bfb90bf0eba4630d193f69dc3cc394c908750a
Rafael Gutkowski <nounderline@libred.org>
2025-11-19 15:37:22 +0100

    move dv script to Fish function and add dvm helper
    - Convert bin/dv shell script to fish/functions/dv.fish
    - Add dvm function to diff against default branch (main/master)
    - Smart default branch detection via remote HEAD or local refs

---
+++ fish/functions/dvm.fish
@@ -0,0 +1,18 @@
+function dvm --description "Open Diffview comparing to default branch (main/master)"
+    # Try to get the default branch from remote HEAD
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD 2>/dev/null | sed 's@^refs/remotes/origin/@@')
+
+    # If that fails, try to detect main or master locally
+    if test -z "$default_branch"
+        if git show-ref --verify --quiet refs/heads/main
+            set default_branch main
+        else if git show-ref --verify --quiet refs/heads/master
+            set default_branch master
+        else
+            echo "Error: Could not determine default branch" >&2
+            return 1
+        end
+    end
+
+    dv $default_branch
+end
